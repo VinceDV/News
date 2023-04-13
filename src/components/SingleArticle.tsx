f925304577354ab52eaecfc7ce00a00caaed7bf4
VinceDV <122369775+VinceDV@users.noreply.github.com>
2023-04-13 17:32:53 +0200

    com

--- src/components/SingleArticle.tsx
+++ src/components/SingleArticle.tsx
@@ -9,25 +9,36 @@ interface SingleArticleProps {
 }
 
 const SingleArticle = (props: SingleArticleProps) => {
-    const navigate = useNavigate();
-    const location = useLocation();
+  const navigate = useNavigate();
+  const location = useLocation();
 
-const handleClick = () => {
-    navigate("/SpecificArticle")
-}
+  const handleClick = () => {
+    navigate("/SpecificArticle");
+  };
   return (
-    <Col className="col col-sm-6 col-md-3">
-
-    <Card className="mx-5" style={{ width: "25rem" }}>
-      <Card.Img variant="top" src={props.article.imageUrl} />
-      <Card.Body>
-        <Card.Title>{props.article.title}</Card.Title>
-        <Card.Text className="text-truncate">{props.article.summary}</Card.Text>
-        <Button variant="primary" onClick={handleClick}>Read More</Button>
-      </Card.Body>
-    </Card>
+    <Col className="col col-sm-6 col-md-3 mb-5">
+      <Card className="mx-5" style={{ width: "25rem", height: "100%" }}>
+        <Card.Img
+          variant="top"
+          src={props.article.imageUrl}
+          style={{
+            height: "200px",
+            objectFit: "cover",
+            objectPosition: "center",
+          }}
+        />
+        <Card.Body>
+          <Card.Title>{props.article.title}</Card.Title>
+          <Card.Text className="text-truncate">
+            {props.article.summary}
+          </Card.Text>
+          <Button variant="primary" onClick={handleClick}>
+            Read More
+          </Button>
+        </Card.Body>
+      </Card>
     </Col>
-// onClick={() => navigate('/article')}
+    // onClick={() => navigate('/article')}
   );
 };
 
--- src/components/SpecificArticle.tsx
+++ src/components/SpecificArticle.tsx
@@ -2,27 +2,30 @@ import { useState, useEffect } from "react";
 import { Article } from "../interfaces/Article";
 import Col from "react-bootstrap/esm/Col";
 import Card from "react-bootstrap/Card";
-import Button from "react-bootstrap/Button"
+import Button from "react-bootstrap/Button";
 import { useNavigate, useLocation } from "react-router-dom";
-import { Routes, Route, useParams } from 'react-router-dom';
+import { Routes, Route, useParams } from "react-router-dom";
 
 const SpecificArticle = (): JSX.Element => {
-  const [articles, setArticles] = useState<Article>([]);
-  const navigate = useNavigate();
-const location = useLocation();
+  
+    const [article, setArticle] = useState<Article | null > (null);
+    const navigate = useNavigate();
+    const location = useLocation();
+    const params = useParams();
 
-const handleClick = () => {
-    navigate("/")
-}
+    const handleClick = () => {
+        navigate("/");
+    };
+  
   const fetchArticles = async () => {
     try {
       let response = await fetch(
-        `https://api.spaceflightnewsapi.net/v3/articles/:${}`
+        `https://api.spaceflightnewsapi.net/v3/articles/${params.id}`
       );
       if (response.ok) {
         let data = await response.json();
         console.log(data);
-        setArticles(data);
+        setArticle(data);
       } else {
         alert("response NOT ok");
       }
@@ -38,18 +41,19 @@ const handleClick = () => {
   return (
     <>
       <Col className="col col-sm-12">
+        {article &&
         <Card className="mx-5" style={{ width: "25rem" }}>
-          <Card.Img variant="top" src={props.article.imageUrl} />
+          <Card.Img variant="top" src={article.imageUrl} />
           <Card.Body>
-            <Card.Title>{props.article.title}</Card.Title>
+            <Card.Title>{article.title}</Card.Title>
             <Card.Text className="text-truncate">
-              {props.article.summary}
+              {article.summary}
             </Card.Text>
             <Button variant="primary" onClick={handleClick}>
               Go Back
             </Button>
           </Card.Body>
-        </Card>
+        </Card>}
       </Col>
     </>
   );
